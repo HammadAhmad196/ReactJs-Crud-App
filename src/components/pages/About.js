297310407f64771558d3c117413fcdc7baad32b1
unknown <hammadajmad196@gmail.com>
2020-10-18 11:51:27 +0500

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class About extends React.Component{
+    render() {
+        return(
+            <div className="container">
+                <div className="py-4">
+                    <h1>About Page</h1>
+                        <p className="lead">React is an open-source, front end, JavaScript library
+                        for building user interfaces or UI components. It is maintained by Facebookand
+                        a community of individual developers and companies.React can be used as a base
+                        in the development of single-page or mobile applications.
+                        </p> 
+
+                        <p className="lead">React is an open-source, front end, JavaScript library
+                        for building user interfaces or UI components. It is maintained by Facebookand
+                        a community of individual developers and companies.React can be used as a base
+                        in the development of single-page or mobile applications.
+                        </p> 
+
+                        <p className="lead">React is an open-source, front end, JavaScript library
+                        for building user interfaces or UI components. It is maintained by Facebookand
+                        a community of individual developers and companies.React can be used as a base
+                        in the development of single-page or mobile applications.
+                        </p> 
+                </div>
+            </div>
+        );
+    }
+}
+export default About;
